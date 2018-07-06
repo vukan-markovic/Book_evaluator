15cb6779acc1ab59c0269681c0301524c4605d70
Вукан <30355243+Vukan-Markovic@users.noreply.github.com>
2018-07-06 19:55:40 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -19,16 +19,23 @@ def apology(message, code = 400):
     # return rendered apology.html page
     return render_template("apology.html", top = code, bottom = escape(message)), code
 
-# function to require login
+"""
+Decorate route to require login.
+http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+"""
 def login_required(f):
-
-    """
-    Decorate routes to require login.
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
-    """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+# Decorate route to check if user is login. Created to don't allow user to go to login or register page through URL if it is login.
+def logged(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") != None:
+            return redirect("/")
         return f(*args, **kwargs)
     return decorated_function
